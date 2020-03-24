9dc579ead10e381594e079e01caa27f4105da8bd
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-24 12:20:55 -0400

    added sorting by app/rating

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const express = require('express');
+const app = express();
 const playstore = require('./playstore');
 
-
-
-const app = express();
-
+// apps endpoint
 app.get('/apps', (req, res) => {
-  res
-    .status(200)
-    .json(playstore);
+  const { sort, filter } = req.query;
+  let apps = [...playstore];
 
+  if (sort) {
+    if (sort === 'rating') {
+
+      // sort in descending order of Rating
+      apps.sort((a, b) => {
+        if (a['Rating'] > b['Rating']) return -1;
+        return 1;
+      });
+    
+    } else if (sort === 'app') {
+      
+      // sort in ascending order of App
+      apps.sort((a, b) => {
+        if (a['App'] < b['App']) return -1;
+        return 1;
+      });
+
+    } else {
+      res.status(400).send(`'${sort}' provided for sort. Expected 'rating' or 'app'`);
+    }
+  }
+
+  if (filter) {
+    console.log(filter);
+  }
+
+  res.status(200).json(apps);
 });
 
-
-
-
-
-
-app.listen('8080');
-
-
-
-
+// start listening
+app.listen('8080', () => console.log('Server live on :8080'));
